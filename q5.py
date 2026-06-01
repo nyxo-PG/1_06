d541005dd5487a2158ee4bd924e4c12c19a113ef
nyxo-PG <pasindugimhana512@gmail.com>
2026-06-01 11:59:00 +0530

    new question

---
+++ q5.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+if age < 18:
+    print("Not Eligible.")
+else:
+    print("Eligible.")
---
+++ q6.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+x = num % 2
+if x == 0:
+    print("Even")   
+else:
+    print("Odd")
